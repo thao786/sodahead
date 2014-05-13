c190e498fada1b739cabbe15931339712bedd426
thao ng <roseskindergarten@gmail.com>
2014-05-12 23:53:00 -0400

    create def statements

--- src/sodahead/render.clj
+++ src/sodahead/render.clj
@@ -10,6 +10,17 @@
 			cach-able-vector 	(map pe/morph-into-code chunks)]
 		5))
 
+(defn get-def-str 
+	"return string (def variable value)"
+	[params key-word]
+	(let [value 	(params key-word)
+		 variable	(name key-word)]
+		(str " (def " variable " " value ") ")))
 
 (defn mk-defs [params]
-	())
+	(if-let [key-list (keys params)]
+		(let [key-vector 	(vec key-list)
+			 def-vector 	(map (partial get-def-str params) key-vector)]
+			(apply str def-vector))))
+
+
