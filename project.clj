84d954654cd9065e2a4fb2b37eedd76a03f62bff
thao ng <roseskindergarten@gmail.com>
2014-05-14 17:58:05 -0400

    add description

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject sodahead "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "a small front end engine for Clojure inspired by .NET Razor"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
