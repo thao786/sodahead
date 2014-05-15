ed60e9a3192fb48f84543365d3be6ac25fa2a80e
thao ng <roseskindergarten@gmail.com>
2014-05-15 00:02:38 -0400

    partition functions

--- src/sodahead/render.clj
+++ src/sodahead/render.clj
@@ -5,9 +5,6 @@
 
 (def ns-list (atom {}))
 
-(defn gen-ns-header []
-	(str "(ns " (gensym "sodahead") ")\n"))
-
 (defn bloc-or-expr
 	"return true if this chunk is a block or expression"
 	[chunks chunk]
@@ -19,7 +16,7 @@
 
 (defn gen-ns-file
 	""
-	[original-text key-str]
+	[original-text key-str new-ns]
 	(let 	[chunks 	(-> original-text (pe/get-included) (p/chop))
 			ns-block-index 	(some (partial bloc-or-expr chunks) chunks)
 			ns-block 	(get chunks ns-block-index)
@@ -27,22 +24,27 @@
 			body-chunks 	(remove #(= ns-block-index (.indexOf chunks %)) chunks)
 			code-vector 	(map pe/morph-into-code body-chunks)
 			body-code 	(pe/wrap-do code-vector key-str)
-			body-str 	(str (gen-ns-header) ns-block-content body-code)]
+			body-str 	(str "(ns " new-ns ")\n" ns-block-content body-code)]
 		body-str))
 
 (defn assemble
-	"if first time, create file namespace containing function wt such params"
-	[file params]
+	"create file namespace containing function wt such params"
+	[text params]
 	(let 	[param-val 	(vals params)
 			param-key 	(keys params)
 		 	key-str 	(apply str (for [x (keys params)] 
 		 						(str (name x) " ")))
-		 	]
-		))
+		 	new-ns 	(gensym "sodahead")
+		 	new-ns-file (str "src/" new-ns ".clj")
+		 	dummy 	(spit new-ns-file (gen-ns-file text key-str new-ns))
+		 	dummy 	(load "temp")]
+		new-ns))
 
 (defn render
-	"if first time, create file namespace containing function wt such params"
 	[file params]
 	(if-let [file-ns	(get ns-list file)]
 		(apply file-ns/render (vals params))
-		()))
+		(if-let [text (try (slurp file) (catch Exception e nil))]
+			(let [new-ns (assemble text params)]
+				(apply new-ns/render (vals params)))
+			(str file " cannot be found by slurp."))))
